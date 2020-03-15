db368da8f076a52e877593374b6d043782f23a40
babanazar <g.babanazar@gmail.com>
2020-03-16 01:33:55 +0300

    Added failsafe plugin

--- src/test/java/guru/springframework/recipe/repository/UnitOfMeasureRepositoryIT.java
+++ src/test/java/guru/springframework/recipe/repository/UnitOfMeasureRepositoryIT.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-class UnitOfMeasureRepositoryTest {
+class UnitOfMeasureRepositoryIT {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
